0ba878d87ab5314237ed4659bd24ef37767e892c
elvis-kimutai <cheruiyotkim17@gmail.com>
2024-01-15 07:48:16 -0800

    asyncio.Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+  Asynchronous coroutine that waits for a
+  random delay between 0 and max_delay seconds.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    return:
+        a asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
